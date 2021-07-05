e4514bbc7439e91f6676b5d5bd840fe653ca3413
PhilipMay <philip@may.la>
2021-07-05 14:32:16 +0200

    add __all__ to __init__.py

--- hpoflow/__init__.py
+++ hpoflow/__init__.py
@@ -2,4 +2,6 @@
 # This software is distributed under the terms of the MIT license
 # which is available at https://opensource.org/licenses/MIT
 
-from hpoflow.version import __version__  # noqa: F401
+from hpoflow.version import __version__
+
+__all__ = ["__version__"]
