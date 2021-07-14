6815162c625d879f1f6482efbcb5a5b317e84294
PhilipMay <philip@may.la>
2021-07-14 09:58:10 +0200

    improve utils test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@
 # This software is distributed under the terms of the MIT license
 # which is available at https://opensource.org/licenses/MIT
 
+import pytest
 
 from hpoflow.utils import func_no_exception_caller
 
@@ -10,7 +11,8 @@ def test_func_no_exception_caller(caplog):
     def func_with_exception(x, y, z=8):
         raise Exception("Test exception.")
 
-    func_no_exception_caller(func_with_exception, 3, z=9, y="hello")
+    with pytest.warns(RuntimeWarning):
+        func_no_exception_caller(func_with_exception, 3, z=9, y="hello")
 
     assert "Exception raised calling func_with_exception!" in caplog.text
     assert "With args: (3,) kwargs: {'z': 9, 'y': 'hello'}" in caplog.text
