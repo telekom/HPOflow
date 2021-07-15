1b8c26456ddb28c874fd23536991c2fa68a3e4b8
PhilipMay <philip@may.la>
2021-07-15 18:27:11 +0200

    return None from func_no_exception_caller if ex.

--- hpoflow/utils.py
+++ hpoflow/utils.py
@@ -16,12 +16,17 @@ def func_no_exception_caller(func, *args, **kwargs):
     """Delegate the function call and log exceptions.
 
     This function catches all exceptions and just logs them.
+
+    Returns:
+        The function result or ``None`` is an exception was raised.
     """
+    result = None
     try:
-        func(*args, **kwargs)
+        result = func(*args, **kwargs)
     except Exception as e:
         error_msg = "Exception raised calling {}! With args: {} kwargs: {} exception: {}".format(
             func.__name__, args, kwargs, e
         )
         _logger.error(error_msg, exc_info=True)
         warnings.warn(error_msg, RuntimeWarning)
+    return result
