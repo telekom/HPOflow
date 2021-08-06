d0fc845508598bb6dec739230593afa8005489b5
PhilipMay <philip@may.la>
2021-08-06 07:27:30 +0200

    bump version to 0.1.3rc1

--- hpoflow/__init__.py
+++ hpoflow/__init__.py
@@ -12,7 +12,7 @@ from lazy_imports import LazyImporter
 
 # Versioning follows the `Semantic Versioning Specification <https://semver.org/>`__ and
 # `PEP 440 -- Version Identification and Dependency Specification <https://www.python.org/dev/peps/pep-0440/>`__.  # noqa: E501
-__version__ = "0.1.2"
+__version__ = "0.1.3rc1"
 
 _import_structure = {
     "mlflow": [
