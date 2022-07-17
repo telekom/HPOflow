fb4c1f7c2bb633b2aef1edd30f0482df92047877
Philip May <philip@may.la>
2022-07-17 22:12:27 +0200

    add all packages in setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,12 @@ extras_require = {
     "doc": ["sphinx", "sphinx_rtd_theme", "myst_parser", "sphinx_copybutton"],
 }
 
+# add "all"
+all_extra_packages = list(
+    {package_name for value in extras_require.values() for package_name in value}
+)
+extras_require["all"] = all_extra_packages
+
 
 def get_version():
     """Read version from ``__init__.py``."""
